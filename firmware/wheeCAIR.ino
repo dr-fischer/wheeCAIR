ec2a8ca33abc10745983213cb4dc1b158d416dc4
Al Fischer <alphonse@users.noreply.github.com>
2021-05-28 22:22:06 -0400

    add comments

--- firmware/wheeCAIR.ino
+++ firmware/wheeCAIR.ino
@@ -1,3 +1,7 @@
+// wheeCAIR Air Quality Sensor Code
+// (c) 2019-2021 Al Fischer, dfischer@wcu.edu
+// written for Teensy 3.5 + BME680 (Seeed Studio) + Honeywell HPMA PM sensor
+
 String deviceID = "MACK"; // change length of array in 3 places
 
 #include "wheeCAIR.h"
@@ -6,7 +10,12 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(33, OUTPUT);
   turnSensorsOn();
+  
+  ////////// set the measurement interval (RtcTimer) depending on use case
+  ////////// for battery power, 10 minutes gives a batter life of ~1 month
+  ////////// for mains power, 10 seconds is a good value
   alarm.setRtcTimer(0, 0, 10); // h, m, s
+    
   pinMode(LED_BUILTIN, OUTPUT);
   sdCard.spiClockPin(chipSelect);
   createFileName(year(), month(), day()).toCharArray(dataFileName, 16);
@@ -24,7 +33,7 @@ void loop() {
   float pavg = performPMReading();
   dataString = printData(deviceID, bme.temperature, bme.humidity, bme.pressure, bme.gas_resistance, pavg);
   delay(100);
-//  Serial.println(dataString);
+//  Serial.println(dataString); // uncomment to use serial port
   delay(1000);
 //  Serial.println(dataFileName);
   writeFile(dataFileName, dataString);
